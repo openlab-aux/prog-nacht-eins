50ef017c09cf111a7d4555b9b64df55ea19f971e
lukasepple <git@lukasepple.de>
2016-03-13 00:11:58 +0100

    prolog: init

---
+++ prolog/facts.pl
@@ -0,0 +1,9 @@
+how_many_prolog_developers_does_it_take_to_change_a_lightbulb. /* Because it needs a joke */
+
+prolog_is_great. /* true. */
+
+age(lukas,15).
+age(prolog,44).
+
+wtf(foo, 123, bla).
+wtf(nein).
