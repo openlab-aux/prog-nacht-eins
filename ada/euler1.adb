9b05c59dd60506a89080758b8deebfb3b6ff0386
Adam Hirst <ahirst@cedint.upm.es>
2016-03-13 06:03:40 +0100

    Cleared up some comments for Euler1.

--- ada/euler1.adb
+++ ada/euler1.adb
@@ -1,16 +1,18 @@
-with Ada.Integer_Text_IO;
-use Ada.Integer_Text_IO;
+with Ada.Integer_Text_IO; -- Importing a module.
+use Ada.Integer_Text_IO; -- Using its namespace.
 
 -- Find the sum of all the multiples of 3 or 5 below 1000.
+-- We will use a simple "sieving" algorithm.
 procedure Euler1 is
-  -- We will use a simple "sieving" algorithm.
+  -- We first must define the local variables we will need.
   Sieve : array(1 .. 999) of Boolean; -- "Below 1000" means 1 to 999.
   Sum   : Integer := 0; -- The sum will be built up progressively here.
 begin
   -- The Sieve is just a boolean array, where True denotes whether the
   -- element's corresponding index is indeed a multiple of 3 or 5.
-  -- We initialise its elements first to False.
+  -- We initialise all its elements first to False.
   Sieve := (others => False);
+  -- I haven't yet found a way to do "native" stridden array slices...
   -- Set to True all elements divisible by 3.
   for i in Integer range 1 .. 999/3 loop
     Sieve(3*i) := True;
